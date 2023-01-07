a81ae9fdd399bf80a8412192b02df421654372aa
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-01-08 06:53:34 +0700

    add morgan and set global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ConfigService } from "@nestjs/config";
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
+import * as morgan from "morgan";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ async function bootstrap() {
   
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.setGlobalPrefix('api');
+  app.use(morgan(':method :url STATUS: :status :res[content-length] - :response-time ms'));
   
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
   const port = parseInt(configService.get('PORT')) || 8000;
